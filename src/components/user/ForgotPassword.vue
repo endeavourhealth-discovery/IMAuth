ee056fda33796199310f3d77c2f8c0134c3f2abb
matt-voror <matt@voror.co.uk>
2022-08-17 18:38:30 +0100

    forgotpassword swapped to composition

--- src/components/user/ForgotPassword.vue
+++ src/components/user/ForgotPassword.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row align-items-center">
     <Card class="flex flex-column justify-content-sm-around align-items-center recovery-card">
       <template #header>
-        <i class="pi pi-fw pi-user icon-header" aria-hidden="true" />
+        <i class="fa-solid fa-user icon-header" aria-hidden="true"></i>
       </template>
       <template #title> Account Recovery: <br /><br />Password Reset </template>
       <template #content>
@@ -17,71 +17,65 @@
         </div>
       </template>
       <template #footer>
-        <small
-          >Already have a recovery code?
-          <a id="password-submit-link" class="footer-link" @click="$router.push({ name: 'ForgotPasswordSubmit' })">Submit Code</a></small
-        >
+        <small>
+          Already have a recovery code?
+          <a id="password-submit-link" class="footer-link" @click="router.push({ name: 'ForgotPasswordSubmit' })">Submit Code</a>
+        </small>
         <br />
         <br />
-        <small
-          >If you have forgotten your username,<br />
-          please contact an admin</small
-        >
+        <small>
+          If you have forgotten your username,<br />
+          please contact an admin
+        </small>
       </template>
     </Card>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 import AuthService from "@/services/AuthService";
 import { SweetAlertResult } from "sweetalert2";
+import Swal from "sweetalert2";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
-export default defineComponent({
-  name: "ForgotPassword",
-  data() {
-    return {
-      username: ""
-    };
-  },
-  methods: {
-    handleSubmit(): void {
-      this.$swal
-        .fire({
-          icon: "warning",
-          title: "Warning",
-          text: "Reset password for account: " + this.username,
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          confirmButtonText: "Reset Password"
-        })
-        .then((result: SweetAlertResult) => {
-          if (result.isConfirmed) {
-            AuthService.forgotPassword(this.username).then(res => {
-              if (res.status === 200) {
-                this.$swal
-                  .fire({
-                    icon: "success",
-                    title: "Success",
-                    text: "Password has been reset for account: " + this.username + ". An email has been sent with a recovery code."
-                  })
-                  .then(() => {
-                    this.$store.commit("updateRegisteredUsername", this.username);
-                    this.$router.push({ name: "ForgotPasswordSubmit" });
-                  });
-              } else {
-                this.$swal.fire({
-                  icon: "error",
-                  title: "Error",
-                  text: res.message + ". Check username is correct."
-                });
-              }
-            });
-          }
-        });
+const router = useRouter();
+const store = useStore();
+
+let username = ref("");
+
+function handleSubmit(): void {
+  Swal.fire({
+    icon: "warning",
+    title: "Warning",
+    text: "Reset password for account: " + username.value,
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    confirmButtonText: "Reset Password"
+  }).then((result: SweetAlertResult) => {
+    if (result.isConfirmed) {
+      AuthService.forgotPassword(username.value).then(res => {
+        if (res.status === 200) {
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "Password has been reset for account: " + username.value + ". An email has been sent with a recovery code."
+          }).then(() => {
+            store.commit("updateRegisteredUsername", username.value);
+            router.push({ name: "ForgotPasswordSubmit" });
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: res.message + ". Check username is correct."
+          });
+        }
+      });
     }
-  }
-});
+  });
+}
 </script>
 
 <style scoped>
