5db00ad12da075b3d6c86f77c2e63a97df3a5017
matt-voror <matt@voror.co.uk>
2022-08-17 19:30:27 +0100

    converted forgotpasswordsubmit to composition

--- src/components/user/ForgotPasswordSubmit.vue
+++ src/components/user/ForgotPasswordSubmit.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row align-items-center">
     <Card class="flex flex-column justify-content-sm-around align-items-center recovery-card">
       <template #header>
-        <i class="pi pi-fw pi-user icon-header" aria-hidden="true" />
+        <i class="fa-solid fa-user icon-header" aria-hidden="true" />
       </template>
       <template #title> Account Recovery: <br /><br />Submit Password Reset Code </template>
       <template #content>
@@ -27,10 +27,9 @@
             <InlineMessage v-if="passwordStrength === 'medium'" severity="success">Password Strength: Medium</InlineMessage>
             <InlineMessage v-if="passwordStrength === 'weak'" severity="warn">Password Strength: Weak</InlineMessage>
             <InlineMessage v-if="passwordStrength === 'fail' && newPassword1 !== ''" severity="error">Invalid Password</InlineMessage>
-            <small id="password-help"
-              >Password min length 8 characters. Improve password strength with a mixture of UPPERCASE, lowercase, numbers and special characters
-              [!@#$%^&*].</small
-            >
+            <small id="password-help">
+              Password min length 8 characters. Improve password strength with a mixture of UPPERCASE, lowercase, numbers and special characters [!@#$%^&*].
+            </small>
           </div>
           <div class="field">
             <label for="fieldPassword2">Confirm New Password</label>
@@ -43,7 +42,7 @@
         </div>
       </template>
       <template #footer>
-        <small>Request a new code <a id="password-submit-link" class="footer-link" @click="$router.push({ name: 'ForgotPasswordSubmit' })">here</a></small>
+        <small>Request a new code <a id="password-submit-link" class="footer-link" @click="router.push({ name: 'ForgotPassword' })">here</a></small>
         <br />
         <br />
         <small>If you have forgotten your username, please contact an admin</small>
@@ -52,97 +51,85 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { mapState } from "vuex";
+<script setup lang="ts">
+import { computed, defineComponent, onMounted, Ref, ref, watch } from "vue";
+import { mapState, useStore } from "vuex";
 import AuthService from "@/services/AuthService";
 import { Helpers, Enums } from "im-library";
 import { SweetAlertResult } from "sweetalert2";
+import Swal from "sweetalert2";
+import { useRouter } from "vue-router";
 const {
   UserMethods: { verifyPasswordsMatch, checkPasswordStrength }
 } = Helpers;
 const { PasswordStrength } = Enums;
 
-export default defineComponent({
-  name: "ForgotPasswordSubmit",
-  computed: mapState(["registeredUsername"]),
-  watch: {
-    code() {
-      this.verifyCode();
-    },
-    newPassword1(newValue) {
-      this.passwordStrength = checkPasswordStrength(newValue);
-    },
-    newPassword2(newValue) {
-      this.passwordsMatch = verifyPasswordsMatch(this.newPassword1, newValue);
-    }
-  },
-  data() {
-    return {
-      code: "",
-      codeVerified: false,
-      username: "",
-      newPassword1: "",
-      newPassword2: "",
-      passwordStrength: PasswordStrength.fail as Enums.PasswordStrength,
-      passwordsMatch: false,
-      showPassword2Notice: false
-    };
-  },
-  mounted() {
-    if (this.registeredUsername && this.registeredUsername !== "") {
-      this.username = this.registeredUsername;
-    }
-  },
-  methods: {
-    setShowPassword2Notice(): void {
-      this.showPassword2Notice = this.passwordsMatch ? false : true;
-    },
+const router = useRouter();
+const store = useStore();
+const registeredUsername = computed(() => store.state.registeredUsername);
 
-    verifyCode(): void {
-      this.codeVerified = /^(?=.{6,})/.test(this.code);
-    },
+let code = ref("");
+let username = ref("");
+let newPassword1 = ref("");
+let newPassword2 = ref("");
+let passwordStrength: Ref<Enums.PasswordStrength> = ref(PasswordStrength.fail);
+let showPassword2Notice = ref(false);
 
-    handleSubmit(): void {
-      if (this.codeVerified && this.username !== "" && this.passwordsMatch && this.passwordStrength !== PasswordStrength.fail) {
-        AuthService.forgotPasswordSubmit(this.username, this.code, this.newPassword1).then(res => {
-          if (res.status === 200) {
-            this.$swal
-              .fire({
-                icon: "success",
-                title: "Success",
-                text: "Password successfully reset",
-                confirmButtonText: "Continue"
-              })
-              .then(() => {
-                this.$router.push({ name: "Login" });
-              });
-          } else if (res.status === 403) {
-            this.$swal
-              .fire({
-                icon: "error",
-                title: "Code Expired",
-                text: "Password reset code has expired. Please request a new code",
-                showCancelButton: true,
-                confirmButtonText: "Request new code"
-              })
-              .then((result: SweetAlertResult) => {
-                if (result.isConfirmed) {
-                  this.$router.push({ name: "ForgotPassword" });
-                }
-              });
-          } else {
-            this.$swal.fire({
-              icon: "error",
-              title: "Error",
-              text: res.message + ". Check input data."
-            });
-          }
-        });
-      }
-    }
+const codeVerified = computed(() => verifyCode(code.value));
+const passwordsMatch = computed(() => verifyPasswordsMatch(newPassword1.value, newPassword2.value));
+
+watch(newPassword1, newValue => {
+  passwordStrength.value = checkPasswordStrength(newValue);
+});
+
+onMounted(() => {
+  if (registeredUsername.value && registeredUsername.value !== "") {
+    username.value = registeredUsername.value;
   }
 });
+
+function setShowPassword2Notice(): void {
+  showPassword2Notice.value = passwordsMatch.value ? false : true;
+}
+
+function verifyCode(code: string): boolean {
+  return /^(?=.{6,})/.test(code);
+}
+
+function handleSubmit(): void {
+  if (codeVerified.value && username.value !== "" && passwordsMatch.value && passwordStrength.value !== PasswordStrength.fail) {
+    AuthService.forgotPasswordSubmit(username.value, code.value, newPassword1.value).then(res => {
+      if (res.status === 200) {
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: "Password successfully reset",
+          confirmButtonText: "Continue"
+        }).then(() => {
+          router.push({ name: "Login" });
+        });
+      } else if (res.status === 403) {
+        Swal.fire({
+          icon: "error",
+          title: "Code Expired",
+          text: "Password reset code has expired. Please request a new code",
+          showCancelButton: true,
+          confirmButtonText: "Request new code"
+        }).then((result: SweetAlertResult) => {
+          if (result.isConfirmed) {
+            router.push({ name: "ForgotPassword" });
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: res.message + ". Check input data."
+        });
+      }
+    });
+  }
+}
 </script>
 
 <style scoped>
