e64df4199144678f49303da5c9f2a5f8387ee973
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-09-14 14:59:26 +0100

    Fix unit test (ESModule)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
-const path = require("path");
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   base: "./",
   plugins: [vue()],
   resolve: {
     dedupe: ["vue"],
-    alias: { "./runtimeConfig": "./runtimeConfig.browser", "@": path.resolve(__dirname, "./src") }
+    alias: { "./runtimeConfig": "./runtimeConfig.browser", "@": fileURLToPath(new URL('src', import.meta.url)) }
   },
   test: {
     globals: true,
